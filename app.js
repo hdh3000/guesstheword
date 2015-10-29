0117b05657f3870f4a02777f0c0cb9f065c43dfa
hdh3000 <henry.hutcheson@gmail.com>
2015-10-29 13:49:06 -0700

    more heroku config

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var port = process.env.PORT || 5000;
+var ip = process.env.IP || "127.0.0.1";
 
+var server = app.listen(port, function () {
+  var host = ip;
+  var port = server.address().port;
   console.log('wordgame listening %s', port);
 });
